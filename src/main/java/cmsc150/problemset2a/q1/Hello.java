88102eb4765fdc6de92cbfba097b7975972c60f9
Akhmed Malsagov <121697209+akhmedmalsagov@users.noreply.github.com>
2023-09-21 20:24:02 -0500

    Update Hello.java

--- src/main/java/cmsc150/problemset2a/q1/Hello.java
+++ src/main/java/cmsc150/problemset2a/q1/Hello.java
@@ -2,8 +2,8 @@ package cmsc150.problemset2a.q1;
 
 public class Hello {
 
-	public static void main(final String[] args) {
-		System.out.println("Not the right string, tests will fail!");
-	}
+    public static void main(String[] args) {
 
+        System.out.println("Hello World!");
+}
 }
